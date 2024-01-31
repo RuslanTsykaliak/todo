c2e1960dfcfd1c23b9d996eada9d919de5e725d4
Mac <mac@MacBook-Pro-2.local>
2024-01-31 17:39:35 +0200

    50-header-css

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,34 +7,32 @@ import { UserButton } from "@clerk/nextjs";
 function Header() {
 
   return (
-    <div className="bg-gray-800 p-4 text-white dark:bg-gray-700 dark:text-white">
+    <div className="bg-gray-800 p-4 text-white sm:text-sm dark:bg-gray-900 dark:text-gray-50">
       <link rel="manifest" href="/manifest.json" />
-      <div className="container mx-auto flex justify-between items-center">
+      <div className="container mx-auto flex flex-col sm:flex-row justify-between items-center py-6">
         <div className="flex items-center flex-grow">
           <NextThemeProvider>
             <ThemeSwitch />
           </NextThemeProvider>
         </div>
-        <h1 className="text-2xl font-extrabold uppercase">
-          <Link href={`/`}>Public Todos </Link>
-          <Link href={`/your`}>| Your Todos</Link>
+        <h1 className="text-xl sm:text-3xl font-extrabold uppercase tracking-wide text-blue-500 dark:text-blue-300">
+          <Link href={`/`} className="mr-2">Public Todos</Link>
+          |
+          <Link href={`/your`} className="ml-2">Your Todos</Link>
         </h1>
         <div className="flex items-center flex-grow justify-end">
-          {/* <Link href={`/sign-up`}>Sign-UP</Link>
-          <Link href={`/sign-in`}>Sing-IN</Link> */}
-          
           <Link
             href={`/add`}
-            className="dark:hover:text-gray-300 font-extrabold uppercase">
+            className="text-md sm:text-lg dark:hover:text-blue-300 font-extrabold uppercase tracking-wide">
             Add Todo
           </Link>
-        <div className="items-end pl-4">
-          <UserButton />
+          <div className="items-end pl-4">
+            <UserButton />
           </div>
         </div>
       </div>
     </div>
-  );
+  )
 }
 
 export default Header;
