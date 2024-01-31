932296258d0eeadf7b9ad6b7b15dbd2b0a25f120
Mac <mac@MacBook-Pro-2.local>
2024-01-31 18:14:36 +0200

    51-header-css

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,23 +7,26 @@ import { UserButton } from "@clerk/nextjs";
 function Header() {
 
   return (
-    <div className="bg-gray-800 p-4 text-white sm:text-sm dark:bg-gray-900 dark:text-gray-50">
+    <div className="bg-gray-800 p-4 text-white text-sm dark:bg-gray-900 dark:text-gray-50">
       <link rel="manifest" href="/manifest.json" />
-      <div className="container mx-auto flex flex-col sm:flex-row justify-between items-center py-6">
-        <div className="flex items-center flex-grow">
+      <div className="container mx-auto flex md:flex-row justify-between items-center py-6">
+        <div className="flex justify-start items-center w-full md:w-auto mb-4 md:mb-0">
           <NextThemeProvider>
             <ThemeSwitch />
           </NextThemeProvider>
         </div>
-        <h1 className="text-xl sm:text-3xl font-extrabold uppercase tracking-wide text-blue-500 dark:text-blue-300">
-          <Link href={`/`} className="mr-2">Public Todos</Link>
-          |
-          <Link href={`/your`} className="ml-2">Your Todos</Link>
-        </h1>
-        <div className="flex items-center flex-grow justify-end">
+        <div className="flex flex-col md:flex-row justify-center items-start md:items-center w-full md:w-auto">
+          <h1 className="text-xl md:text-3xl font-extrabold uppercase tracking-wide text-blue-500 dark:text-blue-300 md:mr-2">
+            <Link href={`/`}>Public Todos</Link>
+          </h1>
+          <h1 className="text-xl md:text-3xl font-extrabold uppercase tracking-wide text-blue-500 dark:text-blue-300 ml-1 md:ml-2">
+            <Link href={`/your`}>Your Todos</Link>
+          </h1>
+        </div>
+        <div className="flex justify-end items-center w-full md:w-auto mt-4 md:mt-0">
           <Link
             href={`/add`}
-            className="text-md sm:text-lg hover:text-blue-500  dark:hover:text-blue-300 font-extrabold uppercase tracking-wide">
+            className="text-md md:text-lg hover:text-blue-500  dark:hover:text-blue-300 font-extrabold uppercase tracking-wide">
             Add Todo
           </Link>
           <div className="items-end pl-4">
@@ -32,7 +35,11 @@ function Header() {
         </div>
       </div>
     </div>
-  )
+)
+
+
+
+
 }
 
 export default Header;
